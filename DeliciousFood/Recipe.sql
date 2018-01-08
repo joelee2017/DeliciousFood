a52dfcccb9b45f3fb3cb69b06488f8bfe87fe965
joelee2017 <as955133@gmail.com>
2018-01-08 08:48:13 +0800

    專題名稱： DeliciousFood SQL-專題建立 UserID Table Recipe Table

---
+++ DeliciousFood/Recipe.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Recipe]
+(
+	[RecipeID] INT NOT NULL PRIMARY KEY, 
+    [UserID] INT NOT NULL, 
+    [FoodName] NVARCHAR(20) NOT NULL, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [CookingTime] INT NULL, 
+    [Amount] INT NULL, 
+    [Photo] VARBINARY(MAX) NULL, 
+    [Tips] NVARCHAR(200) NULL
+)
---
+++ DeliciousFood/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [PassWord] NVARCHAR(100) NOT NULL, 
+    [Gender] NCHAR(10) NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Photo] VARBINARY(MAX) NULL, 
+    [LineID] NVARCHAR(50) NULL
+)
